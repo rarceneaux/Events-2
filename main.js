e42757fc292d477ca07d5ac8b918191bd7738369
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-08-27 23:48:07 -0500

    added data and btns

---
+++ main.js
@@ -0,0 +1,45 @@
+console.log('Events-2');
+
+let teams = [
+  {
+    name:"Titans",
+    img:"https://is4-ssl.mzstatic.com/image/thumb/Purple123/v4/63/2c/34/632c34a2-8e7d-dd78-f55c-6ed5abdee6d0/source/256x256bb.jpg",
+    conference:"AFC",
+    division:"South",
+    madePlayoffsLastSeason:false,
+  },
+  {
+    name:"Texans",
+    img:"https://pbs.twimg.com/profile_images/664402312996200448/5W2Lusba_400x400.jpg",
+    conference:"AFC",
+    division:"South",
+    madePlayoffsLastSeason:true,
+  },
+  {
+    name:"Jaguars",
+    img:"https://pbs.twimg.com/profile_images/664403706822197248/upeZoABV_400x400.png",
+    conference:"AFC",
+    division:"South",
+    madePlayoffsLastSeason:false,
+  },
+  {
+    name:"Rams",
+    img:"https://i.pinimg.com/originals/88/79/a4/8879a4f189033c8f6615d10b895b0046.jpg",
+    conference:"NFC",
+    division:"West",
+    madePlayoffsLastSeason:true,
+  },
+  {
+    name:"Cardinals",
+    img:"https://pbs.twimg.com/profile_images/796560374254288896/mtgjLctY_400x400.jpg",
+    conference:"NFC",
+    division:"West",
+    madePlayoffsLastSeason:false,
+  },
+  {
+    name:"49ers",
+    img:"https://d1si3tbndbzwz9.cloudfront.net/football/team/23/logo.png",
+    conference:"NFC",
+    division:"West",
+    madePlayoffsLastSeason:false,
+  }];
